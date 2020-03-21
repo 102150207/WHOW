e25c0e0bbe0c1885f6525d6a481328ee7e616d24
trinhvovan <vovantrinhdhbkdn@gmail.com>
2020-03-21 18:41:48 +0700

    finish feature authention and authorization - create role and use model

---
+++ api-server/models/role.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const roleSchema =  new Schema({
+    name : {
+        type : String,
+        require : true
+    }
+})
+
+module.exports = mongoose.model('Role',roleSchema);
+
+////
+// const Role = mongoose.model('Role',new mongoose.Schema({
+
+// }))
+// module.exports = Role;
--- api-server/server.js
+++ api-server/server.js
@@ -1,9 +1,77 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const corsOptions = {
+    origin : "http://localhost:8081"
+}
+const db = require('./models');
+const Role = db.role;
 const app = express();
 
+app.use(cors(corsOptions));
 
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, ()=>{
+app.get("/",(req,res)=>{
+    res.json({message : "Welcome to my application !!"});
+})
+
+db.mongoose
+    .connect(`mongodb://root:root@localhost:27017/db_whow`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+ const initial = () => {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+        new Role({
+            name: "user"
+        }).save(err => {
+            if (err) {
+            console.log("error", err);
+            }
+            console.log("added 'user' to roles collection");
+        });
+
+        new Role({
+            name: "moderator"
+        }).save(err => {
+            if (err) {
+            console.log("error", err);
+            }
+
+            console.log("added 'moderator' to roles collection");
+        });
+
+        new Role({
+            name: "admin"
+        }).save(err => {
+            if (err) {
+            console.log("error", err);
+            }
+
+            console.log("added 'admin' to roles collection");
+        });
+        }
+    });
+}
+
+require('./routers/auth.routes')(app);
+require('./routers/user.routes')(app);
+
+app.listen(8081, ()=>{
     console.log("start server successfull !!")
 })
