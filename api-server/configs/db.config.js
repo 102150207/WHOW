8cd69fbfe847e86e8a5294e287c1846c891d79b3
trinhvovan <vovantrinhdhbkdn@gmail.com>
2020-03-22 10:45:09 +0700

    add post- add localhost

--- api-server/configs/db.config.js
+++ api-server/configs/db.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     HOST: "localhost",
+    HOST: "localhost",
+    PORT: 27017,
     PORT: 27017,
     DB: "db_whow",
     DATABASEUSERNAME : 'root',
-    DATABASEPASSWORD : 'root'
+    DATABASEPASSWORD : 'root',
+    DATABASEPASSWORD : 'root',
 };
